be2c795ab6ba3196ab5897fafecd76f90640bafe
yindaheng98 <yindaheng98@163.com>
2020-12-29 20:00:11 +0800

    添加了一个用于下载远程仓库的脚本

---
+++ getrepo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#本脚本用于下载远程仓库
+URL=$1 #仓库地址
+DIR=$2 #仓库目录
+PRE=$(pwd)
+rm -rf $DIR
+mkdir $DIR
+git clone --mirror $URL $DIR/.git #下载仓库中的所有branch
+cd $DIR
+git config --bool core.bare false
+cd $PRE
