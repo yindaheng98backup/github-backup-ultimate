604579cbd081c071bfdf25ac2fbd5d68b2914e63
yindaheng98 <yindaheng98@163.com>
2020-12-30 18:11:45 +0800

    增加了对备份脚本的备份

---
+++ run_src.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+SRC_REPO=$1
+SRC_BRANCH=$2
+BKP_REPO=$3
+BKP_BRANCH=$4
+git clone -b $SRC_BRANCH $SRC_REPO ./src
+git clone -b $BKP_BRANCH $BKP_REPO ./bkp
+cd ./bkp
+git remote add src ../src
+git checkout src/$SRC_BRANCH
+git branch -d $BKP_BRANCH
+git checkout -b $BKP_BRANCH
+git push --set-upstream origin $BKP_BRANCH
+cd ..
+rm -rf ./src
+rm -rf ./bkp
