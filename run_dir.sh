a84c4870cfc7167528b728c68367ad39bafb8ad3
yindaheng98 <yindaheng98@163.com>
2020-12-30 23:36:22 +0800

    用户名和强制推送的功能完成

--- run_dir.sh
+++ run_dir.sh
@@ -5,7 +5,7 @@ GH_TOKEN=$2
 GE_USER=$3
 GE_TOKEN=$4
 REPOS_URL=https://api.github.com/users/$GH_USER/repos?access_token="$GH_TOKEN"
-GE_PREFIX='https://'$GE_TOKEN'@gitee.com/'$GE_USER
+GE_PREFIX='https://'$GE_USER':'$GE_TOKEN'@gitee.com/'$GE_USER
 curl -s $REPOS_URL | jq -c '.[].clone_url' | while read URL; do
     echo "开始备份"$URL
     rm -rf ./main && mkdir ./main #下载待备份主仓库
@@ -13,7 +13,7 @@ curl -s $REPOS_URL | jq -c '.[].clone_url' | while read URL; do
     REPO_NAME=$(eval "echo $URL | sed 's/.*'$USER'\/\(.*\).git$/\1/g'")
     ./getrepo.sh $CLONE_URL ./main
     cd ./main
-    git push -u $GE_PREFIX'/'$REPO_NAME --all origin -f #直接强推到gitee
+    git push --all -f -u $GE_PREFIX'/'$REPO_NAME #直接强推到gitee
     #TODO: 仓库不存在时创建
     #TODO: 仓库私有情况与github一致
     cd ..
