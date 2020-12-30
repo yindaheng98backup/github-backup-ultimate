11b0e31416af2a7b9d6723e465e614da7c79e5d0
yindaheng98 <yindaheng98@163.com>
2020-12-30 23:58:07 +0800

    能获取私有仓库了

--- run_dir.sh
+++ run_dir.sh
@@ -4,13 +4,13 @@ GH_USER=$1
 GH_TOKEN=$2
 GE_USER=$3
 GE_TOKEN=$4
-REPOS_URL=https://api.github.com/users/$GH_USER/repos?access_token="$GH_TOKEN"
+REPOS_URL='https://api.github.com/user/repos?access_token='$GH_TOKEN'&type=owner'
 GE_PREFIX='https://'$GE_USER':'$GE_TOKEN'@gitee.com/'$GE_USER
 curl -s $REPOS_URL | jq -c '.[].clone_url' | while read URL; do
     echo "开始备份"$URL
     rm -rf ./main && mkdir ./main #下载待备份主仓库
     CLONE_URL=$(eval "echo $URL | sed 's/https:\/\/github.com/https:\/\/$GH_TOKEN@github.com/g'")
-    REPO_NAME=$(eval "echo $URL | sed 's/.*'$USER'\/\(.*\).git$/\1/g'")
+    REPO_NAME=$(eval "echo $URL | sed 's/.*'$GH_USER'\/\(.*\).git$/\1/g'")
     ./getrepo.sh $CLONE_URL ./main
     cd ./main
     git push --all -f -u $GE_PREFIX'/'$REPO_NAME #直接强推到gitee
