2fc1bf9259bfe499b60e9fa5953a269f75f31069
yindaheng98 <yindaheng98@163.com>
2020-12-30 15:01:35 +0800

    完善注释

--- run.sh
+++ run.sh
@@ -4,24 +4,26 @@ GH_TOKEN=$4
 
 PACKREPO_URL=$2
 PACKREPO_BRANCH=$3
-rm -rf ./.git
-rm -rf ./backup
-git clone -b $PACKREPO_BRANCH $PACKREPO_URL ./backup
-cd ./backup
-rm -rf .git
+rm -rf ./.git                                             #删除backup仓库的.git结构
+rm -rf ./backup_repo                                      #删除backup_repo文件夹以免产生冲突
+git clone -b $PACKREPO_BRANCH $PACKREPO_URL ./backup_repo #下载备份汇总仓库到backup_repo文件夹
+cd ./backup_repo                                          #进入备份汇总仓库
+rm -rf ./.git                                             #删除备份汇总仓库原有的.git结构以免产生错误
 
 function backup_repo() {
     URL=$1
     echo "开始备份"$URL
+    rm -rf ./main && mkdir ./main
+    rm -rf ./bkup && mkdir ./bkup
+    ../getrepo.sh $CLONE_URL ./main #下载待备份主仓库
     CLONE_URL=$(eval "echo $url | sed 's/https:\/\/github.com/https:\/\/$GH_TOKEN@github.com/g'")
     REPO_NAME=$(eval "echo $url | sed 's/.*'$USER'\/\(.*\).git$/\1/g'")
-    if [ -x "$REPO_NAME.tar.gz" ]; then
-        rm -rf ./main && mkdir ./main
-        rm -rf ./bkup && mkdir ./bkup
-        tar zxvf $REPO_NAME.tar.gz -C ./main
-        ../getrepo.sh $CLONE_URL ./bkup
-        ../backup.sh ./main ./bkup
-        rm -rf ./main
+    if [ -x "./$REPO_NAME.tar.gz" ]; then    #对应的备份仓库压缩文件存在
+        tar zxvf $REPO_NAME.tar.gz -C ./bkup #解压备份仓库
+        ../backup.sh ./main ./bkup           #执行备份操作
+        rm -rf ./main                        #删除已备份主仓库
+    else                                     #对应的备份仓库压缩文件不存在
+        mv ./main ./bkup                     #直接移动
     fi
     tar -zcvf ./$REPO_NAME.tar.gz ./bkup/.git
     rm -rf ./bkup
