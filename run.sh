8b11f84d0e987d9437e21f186b5a7ce8c46188f3
yindaheng98 <yindaheng98@163.com>
2020-12-30 15:58:26 +0800

    bug fix

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ GH_TOKEN=$4
 
 PACKREPO_URL=$2
 PACKREPO_BRANCH=$3
-rm -rf ./.git                                             #删除backup仓库的.git结构
-rm -rf ./backup_repo                                      #删除backup_repo文件夹以免产生冲突
+rm -rf $(pwd)/.git                                        #删除backup仓库的.git结构
+rm -rf $(pwd)/backup_repo                                 #删除backup_repo文件夹以免产生冲突
 git clone -b $PACKREPO_BRANCH $PACKREPO_URL ./backup_repo #下载备份汇总仓库到backup_repo文件夹
-cd ./backup_repo                                          #进入备份汇总仓库
-rm -rf ./.git                                             #删除备份汇总仓库原有的.git结构以免产生错误
+cd $(pwd)/backup_repo                                     #进入备份汇总仓库
+rm -rf $(pwd)/.git                                        #删除备份汇总仓库原有的.git结构以免产生错误
 
 function backup_repo() {
     URL=$1
@@ -17,15 +17,20 @@ function backup_repo() {
     rm -rf ./bkup && mkdir ./bkup
     CLONE_URL=$(eval "echo $URL | sed 's/https:\/\/github.com/https:\/\/$GH_TOKEN@github.com/g'")
     REPO_NAME=$(eval "echo $URL | sed 's/.*'$USER'\/\(.*\).git$/\1/g'")
-    ../getrepo.sh $CLONE_URL $(pwd)/main     #下载待备份主仓库
-    if [ -x "./$REPO_NAME.tar.gz" ]; then    #对应的备份仓库压缩文件存在
-        tar zxvf $REPO_NAME.tar.gz -C ./bkup #解压备份仓库
-        ../backup.sh $(pwd)/main $(pwd)/bkup #执行备份操作
-        rm -rf ./main                        #删除已备份主仓库
-    else                                     #对应的备份仓库压缩文件不存在
-        mv ./main ./bkup                     #直接移动
+    ../getrepo.sh $CLONE_URL $(pwd)/main          #下载待备份主仓库
+    if [ -x "./$REPO_NAME.tar.gz" ]; then         #对应的备份仓库压缩文件存在
+        tar zxvf $REPO_NAME.tar.gz -C $(pwd)/bkup #解压备份仓库
     fi
-    tar -zcvf ./$REPO_NAME.tar.gz ./bkup/.git
+    if [ -x "./bkup/.git" ]; then            #备份仓库git目录存在
+        ../backup.sh $(pwd)/main $(pwd)/bkup #执行备份操作
+    else                                     #备份仓库git目录不存在
+        rm -rf $(pwd)/bkup                   #删除备份仓库
+        mv $(pwd)/main $(pwd)/bkup           #直接移动
+    fi
+    rm -rf ./main #删除已备份主仓库
+    cd ./bkup
+    tar -zcvf ../$REPO_NAME.tar.gz ./.git
+    cd ..
     rm -rf ./bkup
 }
 
