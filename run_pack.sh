020d790415b576a64b52a98edf0a129cd1082f55
yindaheng98 <yindaheng98@163.com>
2020-12-31 13:58:29 +0800

    能获取私有库了

--- run_pack.sh
+++ run_pack.sh
@@ -11,8 +11,9 @@ cd $(pwd)/backup_repo                                     #进入备份汇总仓
 rm -rf $(pwd)/.git                                        #删除备份汇总仓库原有的.git结构以免产生错误
 
 USER=$1
-REPOS_URL=https://api.github.com/users/$USER/repos?access_token="$GH_TOKEN"
-curl -s $REPOS_URL | jq -c '.[].clone_url' | while read URL; do
+HEADER="Authorization: token $GH_TOKEN"
+REPOS_URL='https://api.github.com/user/repos?type=owner&per_page=100'
+curl -H $HEADER -s $REPOS_URL | jq -c '.[].clone_url' | while read URL; do
     echo "开始备份"$URL
 
     rm -rf ./main && mkdir ./main #下载待备份主仓库
