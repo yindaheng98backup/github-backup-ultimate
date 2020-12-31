d57c476be9417eff6b6ae16773e4cbe7fa811cd0
yindaheng98 <yindaheng98@163.com>
2020-12-31 14:32:19 +0800

    bug fix

--- run_dir.sh
+++ run_dir.sh
@@ -5,10 +5,9 @@ GH_TOKEN=$2
 GE_USER=$3
 GE_TOKEN=$4
 
-HEADER="Authorization: token $GH_TOKEN"
 PUBLIC_REPOS_URL='https://api.github.com/user/repos?visibility=public&affiliation=owner&per_page=100'
 PRIVATE_REPOS_URL='https://api.github.com/user/repos?visibility=private&affiliation=owner&per_page=100'
-curl -H $HEADER -s $PUBLIC_REPOS_URL | jq -c '.[].clone_url' | while read URL; do
+curl -H "Authorization: token $GH_TOKEN" -s $PUBLIC_REPOS_URL | jq -c '.[].clone_url' | while read URL; do
     echo "开始备份公开仓库"$URL
     rm -rf ./main && mkdir ./main #下载待备份主仓库
     CLONE_URL=$(eval "echo $URL | sed 's/https:\/\/github.com/https:\/\/$GH_TOKEN@github.com/g'")
