52a52439b0b013ff067a4c19166aa5ffba032a78
yindaheng98 <yindaheng98@163.com>
2020-12-31 17:31:23 +0800

    统一本地仓备份到远端仓的代码

---
+++ backup_to_remote_repo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#将主仓库备份到一个远端仓库中
+
+MAIN_REPO_LOCAL=$1  #主仓库本地目录
+BKUP_REPO_REMOTE=$3 #备份仓库远端目录
+BKUP_REPO_LOCAL=$2  #备份仓库本地目录
+PREDIR=$(pwd)
+./getrepo.sh $BKUP_REPO_REMOTE $BKUP_REPO_LOCAL #下载仓库
+./backup.sh $MAIN_REPO_LOCAL $BKUP_REPO_LOCAL   #执行备份
+cd $BKUP_REPO_LOCAL
+git push --all -f -u $BKUP_REPO_REMOTE #直接强推到远端
+cd $PREDIR
