1dd42d1f1551b97bf3aa47888ba5af0ca41c07e5
yindaheng98 <yindaheng98@163.com>
2021-01-01 11:48:57 +0800

    可以自动在gitee和gitlab创建修改镜像仓库了

---
+++ repo_modify/gitee.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#创建仓库并返回仓库clone地址
+
+USER=$1
+TOKEN=$2
+REPO_NAME=$3
+PRIVATE=$4
+HEADER='Content-Type: application/json;charset=UTF-8'
+CREATE_URL='https://gitee.com/api/v5/user/repos'
+POST_DATA="{\"access_token\":\"$TOKEN\",\"name\":\"$REPO_NAME\"}"
+POST_DATA=$(echo "$POST_DATA" | jq '. + {private: "false"}')
+if [ $PRIVATE ]; then
+    POST_DATA=$(echo "$POST_DATA" | jq '. + {private: "true"}')
+fi
+curl -X POST --header "$HEADER" -s "$CREATE_URL" -d "$POST_DATA" #新建仓库
+
+MODIFY_URL="https://gitee.com/api/v5/$USER/$REPO_NAME"
+curl -X PATCH --header "$HEADER" -s "$MODIFY_URL" -d "$POST_DATA" #修改已有仓库的private状态
+
+echo 'https://'$USER':'$TOKEN'@gitee.com/'$USER'/'$REPO_NAME
---
+++ repo_modify/gitlab.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#创建仓库并返回仓库clone地址
+
+USER=$1
+TOKEN=$2
+REPO_NAME=$3
+PRIVATE=$4
+HEADER="PRIVATE-TOKEN: $TOKEN"
+CREATE_URL='https://gitlab.com/api/v4/projects?name='$REPO_NAME
+if [ $PRIVATE ]; then
+    CREATE_URL=$CREATE_URL'&visibility=private'
+else
+    CREATE_URL=$CREATE_URL'&visibility=public'
+fi
+curl -X POST --header "$HEADER" -s "$CREATE_URL" #新建仓库
+
+MODIFY_URL='https://gitlab.com/api/v4/projects/'$USER'%2F'$REPO_NAME
+if [ $PRIVATE ]; then
+    MODIFY_URL=$MODIFY_URL'?visibility=private'
+else
+    MODIFY_URL=$MODIFY_URL'?visibility=public'
+fi
+curl -X PUT --header "$HEADER" -s "$MODIFY_URL" #修改已有仓库的private状态
+
+echo 'https://'$USER':'$TOKEN'@gitlab.com/'$USER'/'$REPO_NAME
