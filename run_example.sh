8997ceab651cefe5417c1b59c9d23a1d5aa30ef5
yindaheng98 <yindaheng98@163.com>
2021-01-02 23:23:20 +0800

    让Travis有点喘息的机会

---
+++ run_example.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 随机选择是备份到远端仓库还是备份到Aliyun
+# 因为备份很上传很耗时，不管备份到哪都50分钟内做不完
+# 如果两个一起做那必有一个做不了
+# 于是出此下策
+
+bash -x run_backup_src.sh https://$GH_BKUP_TOKEN@github.com/yindaheng98backup/backup-src master https://$GH_BKUP_TOKEN@github.com/yindaheng98backup/backup master
+if [ $(($RANDOM % 2)) -eq 0 ]; then
+    echo '这次备份到Gitee/Gitlab'
+    REPO_PLUGINS="[\"plugins/make_repo_available/gitee.sh yindaheng98 $GE_TOKEN\",\"plugins/make_repo_available/gitlab.sh yindaheng98 $GL_TOKEN\"]"
+    BACKUP_REPO_LIST=$(./get_backup_repo_list.sh yindaheng98 $GH_TOKEN "$REPO_PLUGINS")
+    bash backup_all_to_remote.sh yindaheng98 $GH_TOKEN $BACKUP_REPO_LIST
+else
+    echo '这次备份到Aliyun OSS'
+    PLUGIN_PATH=$(pwd)'/plugins/remote_filesystem/aliyun_oss'
+    endpoint='oss-cn-hangzhou.aliyuncs.com'
+    backupPath='oss://github-backup'
+    $PLUGIN_PATH/configure.sh $accessKeyID $accessKeySecret $endpoint $backupPath
+    ./run_backup_to_tar.gz.sh $GH_TOKEN $PLUGIN_PATH
+    bash -x run_backup_to_tar.gz.sh yindaheng98 $GH_TOKEN https://$GH_BKUP_TOKEN@github.com/yindaheng98backup/backup backup_repo_pack
+fi
