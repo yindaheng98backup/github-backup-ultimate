92b0b4b99c9554d66cdd861809547ad57c34e267
yindaheng98 <yindaheng98@163.com>
2021-01-02 21:28:27 +0800

    增量式上传下载

--- plugins/remote_filesystem/aliyun_oss/cp.sh
+++ plugins/remote_filesystem/aliyun_oss/cp.sh
@@ -5,5 +5,5 @@ DST=$2
 CONF_PATH=$(dirname ${BASH_SOURCE[0]})
 OSS=$(cat $CONF_PATH/CMD.sh)
 rm $CONF_PATH/ossutil.log
-$OSS cp --loglevel info --recursive -f $SRC $DST
+$OSS cp --loglevel info --recursive --update -f $SRC $DST
 cat $CONF_PATH/ossutil.log
