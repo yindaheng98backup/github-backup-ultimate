50ac2c267016b149f3038c58565bb83ab3962fd3
yindaheng98 <yindaheng98@163.com>
2021-01-02 21:17:26 +0800

    仓库被修改才重打压缩包

--- backup_to_local_tar.gz.sh
+++ backup_to_local_tar.gz.sh
@@ -7,17 +7,21 @@ BKUP_REPO_COMPRESS=$2 #备份仓库压缩包名称
 BKUP_REPO_LOCAL=$3    #备份仓库本地目录
 PREDIR=$(pwd)
 
+changed=''
 rm -rf $BKUP_REPO_LOCAL && mkdir $BKUP_REPO_LOCAL
 if [ -x "$BKUP_REPO_COMPRESS" ]; then               #对应的备份仓库压缩文件存在
     tar zxf $BKUP_REPO_COMPRESS -C $BKUP_REPO_LOCAL #解压备份仓库
 fi
-if [ -x "$BKUP_REPO_LOCAL/.git" ]; then            #备份仓库git目录存在
-    ../backup.sh $MAIN_REPO_LOCAL $BKUP_REPO_LOCAL #执行备份操作
-else                                               #备份仓库git目录不存在
-    rm -rf $BKUP_REPO_LOCAL                        #删除备份仓库
-    cp -r $MAIN_REPO_LOCAL $BKUP_REPO_LOCAL        #直接移动
+if [ -x "$BKUP_REPO_LOCAL/.git" ]; then                       #备份仓库git目录存在
+    changed=$(../backup.sh $MAIN_REPO_LOCAL $BKUP_REPO_LOCAL) #执行备份操作
+else                                                          #备份仓库git目录不存在
+    rm -rf $BKUP_REPO_LOCAL                                   #删除备份仓库
+    cp -r $MAIN_REPO_LOCAL $BKUP_REPO_LOCAL                   #直接移动
+    changed='1'
 fi
-cd $BKUP_REPO_LOCAL
-tar -zcf $BKUP_REPO_COMPRESS ./.git #打压缩包
-cd $PREDIR
+if [ $changed ]; then #如果被修改了
+    cd $BKUP_REPO_LOCAL
+    tar -zcf $BKUP_REPO_COMPRESS ./.git #就重打压缩包
+    cd $PREDIR
+fi
 rm -rf $BKUP_REPO_LOCAL
