64b4f4d4de31f44dfe6cf7f118fc01a11886134d
yindaheng98 <yindaheng98@163.com>
2021-01-08 16:06:41 +0800

    bug fix

--- get_repo_content_from_github.sh
+++ get_repo_content_from_github.sh
@@ -2,7 +2,6 @@
 
 TOKEN=$1   # personal access token
 PARAMS=$2  # GET链接的参数，JSON格式
-CONTENT=$3 #要获取什么内容？
 
 param=''
 #直接用管道符写while循环会开启新进程，使while里面的变量不能传到外部
@@ -14,10 +13,4 @@ while read K; do
 done <<<$(echo $PARAMS | jq -cr 'keys | .[]')
 
 REPOS_URL='https://api.github.com/user/repos?'$param
-REPOS='{}'
-while read URL; do
-    CTT=$(eval "echo $URL | sed 's/https:\/\/github.com/https:\/\/$TOKEN@github.com/g'")
-    REPO_NAME=$(eval "echo $URL | sed 's/.*\/\([^\/]*\).git$/\1/g'")
-    REPOS=$(echo $REPOS | jq -c ". + {\"$REPO_NAME\": \"$CTT\"}")
-done <<<$(curl -H "Authorization: token $TOKEN" -s $REPOS_URL | jq -c $CONTENT)
-echo $REPOS
+echo $(curl -H "Authorization: token $TOKEN" -s $REPOS_URL)
--- get_repo_list_from_github.sh
+++ get_repo_list_from_github.sh
@@ -2,5 +2,10 @@
 
 TOKEN=$1  # personal access token
 PARAMS=$2 # GET链接的参数，JSON格式
-REPOS=$(./get_repo_content_from_github.sh $TOKEN $PARAMS '.[].clone_url')
+REPOS='{}'
+while read URL; do
+    CLONE_URL=$(eval "echo $URL | sed 's/https:\/\/github.com/https:\/\/$TOKEN@github.com/g'")
+    REPO_NAME=$(eval "echo $URL | sed 's/.*\/\([^\/]*\).git$/\1/g'")
+    REPOS=$(echo $REPOS | jq -c ". + {\"$REPO_NAME\": \"$CLONE_URL\"}")
+done <<<$(./get_repo_content_from_github.sh $TOKEN $PARAMS | jq -c '.[].clone_url')
 echo $REPOS
