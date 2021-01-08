b562f126192bd161865338319658c147af73de8e
yindaheng98 <yindaheng98@163.com>
2021-01-08 16:20:28 +0800

    直接从github的信息中获取name

--- get_repo_list_from_github.sh
+++ get_repo_list_from_github.sh
@@ -3,9 +3,9 @@
 TOKEN=$1  # personal access token
 PARAMS=$2 # GET链接的参数，JSON格式
 REPOS='{}'
-while read URL; do
-    CLONE_URL=$(eval "echo $URL | sed 's/https:\/\/github.com/https:\/\/$TOKEN@github.com/g'")
-    REPO_NAME=$(eval "echo $URL | sed 's/.*\/\([^\/]*\).git$/\1/g'")
+while read CONTENT; do
+    REPO_NAME=$(echo $CONTENT | jq -cr '.name')
+    CLONE_URL=$(echo $CONTENT | jq -cr '.clone_url' | sed "s/https:\/\/github.com/https:\/\/$TOKEN@github.com/g")
     REPOS=$(echo $REPOS | jq -c ". + {\"$REPO_NAME\": \"$CLONE_URL\"}")
-done <<<$(./get_repo_content_from_github.sh $TOKEN $PARAMS | jq -c '.[].clone_url')
+done <<<$(./get_repo_content_from_github.sh $TOKEN $PARAMS | jq -c '.[] | {name: .name, clone_url: .clone_url}')
 echo $REPOS
