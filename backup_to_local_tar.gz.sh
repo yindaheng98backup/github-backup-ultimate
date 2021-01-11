7e7a661dd6cd159658ac63fa68e9b062ccd5b64a
yindaheng98 <yindaheng98@163.com>
2021-01-11 13:43:02 +0800

    改好了

--- backup_to_local_tar.gz.sh
+++ backup_to_local_tar.gz.sh
@@ -2,22 +2,22 @@
 
 #将主仓库备份到一个压缩包中
 
-REPO_NAME=$1       #仓库名称
-MAIN_REPO_LOCAL=$2 #主仓库本地目录
-BKUP_REPO_LOCAL=$3 #备份仓库本地目录
-PLUGIN_PATH=$4     #存储插件地址
+REPO_NAME=$1                      #仓库名称
+MAIN_REPO_LOCAL=$2                #主仓库本地目录
+PLUGIN_PATH=$3                    #存储插件地址
+BKUP_REPO_LOCAL=$(pwd)/bkup #备份仓库本地目录
 
 changed=''
 rm -rf $BKUP_REPO_LOCAL && mkdir $BKUP_REPO_LOCAL
-bash -x $PLUGIN_PATH/download.sh $BKUP_REPO_NAME $BKUP_REPO_LOCAL #下载仓库
-if [ -d $BKUP_REPO_LOCAL'/.git' ]; then                           #备份仓库git目录存在
-    changed=$(./backup.sh $MAIN_REPO_LOCAL $BKUP_REPO_LOCAL)      #执行备份操作
-else                                                              #备份仓库git目录不存在
-    rm -rf $BKUP_REPO_LOCAL                                       #删除备份仓库
-    cp -r $MAIN_REPO_LOCAL $BKUP_REPO_LOCAL                       #直接移动
+bash -x $PLUGIN_PATH/download.sh $REPO_NAME $BKUP_REPO_LOCAL #下载仓库
+if [ -d $BKUP_REPO_LOCAL'/.git' ]; then                      #备份仓库git目录存在
+    changed=$(./backup.sh $MAIN_REPO_LOCAL $BKUP_REPO_LOCAL) #执行备份操作
+else                                                         #备份仓库git目录不存在
+    rm -rf $BKUP_REPO_LOCAL                                  #删除备份仓库
+    cp -r $MAIN_REPO_LOCAL $BKUP_REPO_LOCAL                  #直接移动
     changed='1'
 fi
-if [ $changed ]; then                                               #如果被修改了
-    bash -x $PLUGIN_PATH/upload.sh $BKUP_REPO_NAME $BKUP_REPO_LOCAL #就上传仓库
+if [ $changed ]; then                                          #如果被修改了
+    bash -x $PLUGIN_PATH/upload.sh $REPO_NAME $BKUP_REPO_LOCAL #就上传仓库
 fi
 rm -rf $BKUP_REPO_LOCAL
--- run_backup_to_tar.gz.sh
+++ run_backup_to_tar.gz.sh
@@ -9,10 +9,11 @@ if [ "$REPO_LIST" = "{}" ]; then                                #没有仓库可
     exit 0
 fi
 
+MAIN_REPO_LOCAL="$(pwd)/main"
+rm -rf "$MAIN_REPO_LOCAL" #删除主仓库
 while read REPO_NAME; do
     CLONE_URL=$(echo $REPO_LIST | jq -cr ".[\"$REPO_NAME\"]")
-    MAIN_REPO_LOCAL="$(pwd)/main"
-    bash -x ../download_repo.sh "$CLONE_URL" "$MAIN_REPO_LOCAL"                         #下载主仓库
-    bash -x ../backup_to_local_tar.gz.sh "$REPO_NAME" "$MAIN_REPO_LOCAL" "$PLUGIN_PATH" #备份到压缩文件
-    rm -rf "$MAIN_REPO_LOCAL"                                                           #删除主仓库
+    bash -x ./download_repo.sh "$CLONE_URL" "$MAIN_REPO_LOCAL"                         #下载主仓库
+    bash -x ./backup_to_local_tar.gz.sh "$REPO_NAME" "$MAIN_REPO_LOCAL" "$PLUGIN_PATH" #备份到压缩文件
+    rm -rf "$MAIN_REPO_LOCAL"                                                          #删除主仓库
 done <<<$(echo $REPO_LIST | jq -cr 'keys_unsorted | .[]')
