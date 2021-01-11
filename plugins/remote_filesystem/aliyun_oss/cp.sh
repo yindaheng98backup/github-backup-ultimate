95b37b0180fcc36728b2b72fd0075c367a319432
yindaheng98 <yindaheng98@163.com>
2021-01-11 13:35:00 +0800

    不需要log

--- plugins/remote_filesystem/aliyun_oss/cp.sh
+++ plugins/remote_filesystem/aliyun_oss/cp.sh
@@ -4,6 +4,4 @@ SRC=$1
 DST=$2
 CONF_PATH=$(dirname ${BASH_SOURCE[0]})
 OSS=$(cat $CONF_PATH/CMD.sh)
-rm $CONF_PATH/ossutil.log
-$OSS cp --loglevel info --update -f $SRC $DST --jobs 10
-cat $CONF_PATH/ossutil.log
+$OSS cp --update -f $SRC $DST --jobs 10
