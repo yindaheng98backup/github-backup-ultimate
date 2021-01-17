73eb9c853414ca9de2a657b0a48d3736c2cd03fb
yindaheng98 <yindaheng98@163.com>
2021-01-17 21:03:08 +0800

    bug fix

--- plugins/remote_filesystem/git/upload.sh
+++ plugins/remote_filesystem/git/upload.sh
@@ -4,7 +4,7 @@
 REPO_NAME=$1 #仓库名称
 LOCAL=$2     #仓库目录
 CONF_PATH=$(dirname ${BASH_SOURCE[0]})
-PERFIX=$(cat $CONF_PATH/UPLOAD_PERFIX)
+PERFIX=$(cat $CONF_PATH/PERFIX)
 URL="$PERFIX/$REPO_NAME"
 PRE=$(pwd)
 cd $LOCAL
