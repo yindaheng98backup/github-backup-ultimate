eb8ebb2b7fb8535da22d6d90cd64fd4cf89f4816
yindaheng98 <yindaheng98@163.com>
2021-01-17 20:52:54 +0800

    基于git的下载上传完成

---
+++ plugins/remote_filesystem/git/download.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#此脚本用于下载远程仓库
+REPO_NAME=$1 #仓库名称
+LOCAL=$2     #仓库目录
+CONF_PATH=$(dirname ${BASH_SOURCE[0]})
+PERFIX=$(cat $CONF_PATH/DOWNLOAD_PERFIX)
+URL="$PERFIX/$REPO_NAME.git"
+PRE=$(pwd)
+rm -rf $LOCAL
+git clone --mirror $URL $LOCAL/.git #下载仓库中的所有branch
+cd $LOCAL
+git config --bool core.bare false
+cd $PRE
---
+++ plugins/remote_filesystem/git/upload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#此脚本用于下载远程仓库
+REPO_NAME=$1 #仓库名称
+LOCAL=$2     #仓库目录
+CONF_PATH=$(dirname ${BASH_SOURCE[0]})
+PERFIX=$(cat $CONF_PATH/UPLOAD_PERFIX)
+URL="$PERFIX/$REPO_NAME"
+PRE=$(pwd)
+cd $LOCAL
+git push --all -f -u $URL #直接强推到远端
+cd $PRE
